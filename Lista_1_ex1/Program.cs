675ce01d9d3856f0926a2053b489c56c03a5c680
ALUNO <ALUNO@LABHPC08>
2024-02-27 14:44:25 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("digite 1 numero:");
+            n1=int.Parse(Console.ReadLine());
+            Console.WriteLine("digite outro numero:");
+            n2=int.Parse(Console.ReadLine());
+            int soma = n1 + n2;
+            if(soma > 10) {soma=soma+5 }
+            else { soma=soma-7;}
+        }
+    }
+}
